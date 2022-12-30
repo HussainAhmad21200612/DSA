34eecafced858cef00d2d99c8a7786c93f60d377
HussainAhmad21200612 <85171776+HussainAhmad21200612@users.noreply.github.com>
2022-12-30 17:03:38 +0530

    Update Single valued tree.py
    updation of code

--- GFG/POTDs/Single valued tree.py
+++ GFG/POTDs/Single valued tree.py
@@ -1,4 +1,5 @@
-      if root==None:
+def sv(self,root,count):
+        if root==None:
             return True
         left=singlevalued(root.left,count)
         right=singlevalued(root.right,count)
@@ -9,5 +10,9 @@
             return False
         if root.right!=None and root.data!=root.right.data:
             return False
-        count+=1
+        count[0]+=1
         return True
+def singlevalued(self,root):
+      count=[0]
+      self.sv(root,count)
+      return count[0]
