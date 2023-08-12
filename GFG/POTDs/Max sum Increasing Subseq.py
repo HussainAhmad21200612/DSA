c55dc25cbe870dc1edbc37fcb592e3167994ca4f
HUSSAIN AHMAD <hussain109164@gmail.com>
2023-08-12 19:56:18 +0530

    Create Max sum Increasing Subseq.py

---
+++ GFG/POTDs/Max sum Increasing Subseq.py
@@ -0,0 +1,17 @@
+class Solution:
+	def maxSumIS(self, arr, n):
+		# code here
+        max = 0
+        msis = [0 for x in range(n)]
+        for i in range(n):
+            msis[i] = arr[i]
+        for i in range(1, n):
+            for j in range(i):
+                if (arr[i] > arr[j] and
+                    msis[i] < msis[j] + arr[i]):
+                    msis[i] = msis[j] + arr[i]
+        for i in range(n):
+            if max < msis[i]:
+                max = msis[i]
+      
+        return max
