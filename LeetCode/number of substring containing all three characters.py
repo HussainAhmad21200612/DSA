39366421f7706e7ad1b41fa93234fccb9ee70fe4
HUSSAIN AHMAD <hussain109164@gmail.com>
2023-08-25 22:49:27 +0530

    Update number of substring containing all three characters.py

--- LeetCode/number of substring containing all three characters.py
+++ LeetCode/number of substring containing all three characters.py
@@ -3,6 +3,6 @@ class Solution:
         d = {"a": -1, "b": -1, "c": -1}
         ans = 0
         for i, c in enumerate(s):
-            d[c] = i+1
-            ans += min(d["a"], d["b"], d["c"])
+            d[c] = i
+            ans += min(d["a"], d["b"], d["c"])+1
         return ans
