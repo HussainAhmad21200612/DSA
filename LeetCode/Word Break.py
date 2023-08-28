109e0d860c1e0dbaa90a21227af148d93f748bf3
HUSSAIN AHMAD <hussain109164@gmail.com>
2023-08-28 10:04:56 -0700

    Create Word Break.py

---
+++ LeetCode/Word Break.py
@@ -0,0 +1,12 @@
+def wordBreak(s, wordDict):
+    # Complete this function
+
+        dp=[False]*(len(s)+1)
+        dp[len(s)]=True
+        for i in range(len(s)-1,-1,-1):
+            for j in wordDict:
+                if i+len(j)<=len(s) and j==s[i:i+len(j)]:
+                    dp[i]=dp[i+len(j)]
+                if dp[i]:
+                    break
+        return dp[0]
