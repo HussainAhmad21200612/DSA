83a048aa09739efe31dbefef4e6f426a499eefb8
HUSSAIN AHMAD <hussain109164@gmail.com>
2023-08-30 10:50:48 -0700

    Update Minimum Replacements to Sort the Array.py

--- LeetCode/Minimum Replacements to Sort the Array.py
+++ LeetCode/Minimum Replacements to Sort the Array.py
@@ -1,23 +1,13 @@
 class Solution:
-    def minimumReplacement(self, arr: List[int]) -> int:
-        ans = 0
-        temp = arr.copy()
-        n=len(arr)
-        h = {}
-      
-        temp.sort()
-      
-        for i in range(n):
-            h[arr[i]] = i
-      
-        init = 0
-      
-        for i in range(n):
-            if (arr[i] != temp[i]):
-                ans += 1
-                init = arr[i]
-                arr[i], arr[h[temp[i]]] = arr[h[temp[i]]], arr[i]
-                h[init] = h[temp[i]]
-                h[temp[i]] = i
-      
+    def minimumReplacement(self, nums: List[int]) -> int:
+        ans, n = 0, len(nums)
+        mi = nums[-1]
+        for i in range(n - 2, -1, -1):
+            v = nums[i]
+            if v <= mi:
+                mi = v
+                continue
+            k = (v + mi - 1) // mi
+            ans += k - 1
+            mi = v // k
         return ans
