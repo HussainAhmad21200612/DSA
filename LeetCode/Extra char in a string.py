2b459bdf5653b1227bcc94e128a34c1af8bdc98f
HUSSAIN AHMAD <hussain109164@gmail.com>
2023-09-02 21:08:24 +0530

    Create Extra char in a string.py

---
+++ LeetCode/Extra char in a string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        n = len(s)
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        
+        for i in range(1, n + 1):
+            for word in dictionary:
+                if i >= len(word) and s[len(word)-1:i] == word:
+                    dp[i] = min(dp[i], dp[i - len(word)])
+            
+            dp[i] = min(dp[i], dp[i - 1] + 1)
+        
+        return dp[n]
