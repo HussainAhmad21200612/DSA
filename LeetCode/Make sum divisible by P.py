8d8484c882cc6757fb2bb4f272d2fec4fb9e9b44
HUSSAIN AHMAD <hussain109164@gmail.com>
2023-09-04 13:28:55 +0530

    Update Make sum divisible by P.py

--- LeetCode/Make sum divisible by P.py
+++ LeetCode/Make sum divisible by P.py
@@ -2,12 +2,12 @@ class Solution:
     def minSubarray(self, A, p):
         #find the remainder
         need = sum(A) % p 
-        dic = {0:0}
+        dic = {0:-1}
         n = res = len(A)
         c = 0
         
         for i, val in enumerate(A):
-            c = (val) % p
+            c = (c+val) % p
             dic[c] = i
             if (c-need) % p in dic:
                 res = min(res, i - dic[(c-need)%p])
