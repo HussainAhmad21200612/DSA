e1471039b6b8a35ccf4106c625f91ee73f1af8eb
HUSSAIN AHMAD <hussain109164@gmail.com>
2023-09-04 13:08:47 +0530

    Create Make sum divisible by P.py

---
+++ GFG/POTDs/Make sum divisible by P.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubarray(self, A, p):
+        #find the remainder
+        need = sum(A) % p 
+        dic = {0:0}
+        n = res = len(A)
+        c = 0
+        
+        for i, val in enumerate(A):
+            c = (val) % p
+            dic[c] = i
+            if (c-need) % p in dic:
+                res = min(res, i - dic[(c-need)%p])
+            
+        return res if res != n else -1
