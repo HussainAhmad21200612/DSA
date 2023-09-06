6586bad8f74b757f84f6c60ce176d1de7d587392
HUSSAIN AHMAD <hussain109164@gmail.com>
2023-09-06 17:24:58 +0530

    Create Solit Linked Lists in Parts.py

---
+++ LeetCode/Solit Linked Lists in Parts.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def splitListToParts(self, root: ListNode, k: int) -> List[ListNode]:
+        n, cur = 0, root
+        while cur:
+            n += 1
+            cur = cur.next
+        cur = root
+        width, remainder = divmod(n, k)
+        res = [None for _ in range(k)]
+        for i in range(k):
+            head = cur
+            for j in range(width + (i <= remainder) - 1):
+                if cur:
+                    cur = cur.next
+            if cur:
+                cur.next, cur = None, cur.next
+            res[i] = head
+        return res
